17786b15c1cc303e34c4ee571041ed2b94457497
smano-20052002 <smano4570@gmail.com>
2023-09-11 09:10:59 +0530

    changed in monday 11-09-2023 just updating the method in class Bnak

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+interface IPersonalDetails
+{
+    void showDetails();
+}
+interface IAccountDetails
+{
+    void showAccountDetails();
+    void showBalance();
+}
+class Bank : IAccountDetails, IPersonalDetails
+{
+    public void showBalance(){
+        
+    }
+    public void showDetails() { }
+    public void showAccountDetails()
+    {
+
+    }
+}
